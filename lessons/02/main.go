064754663d2d7a4a479646a02158905edd7d3d06
vaclav.dvorak <vaclav.dvorak@eurowag.com>
2020-08-28 16:39:35 +0200

    Remove useless comment

--- lessons/02/main.go
+++ lessons/02/main.go
@@ -6,7 +6,6 @@ import (
 	"time"
 )
 
-//? https://towardsdatascience.com/evolution-of-a-salesman-a-complete-genetic-algorithm-tutorial-for-python-6fe5d2b3ca35
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
