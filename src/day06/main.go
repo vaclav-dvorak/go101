f0636642d421df5d14516f4d8b2b16e8b41b3059
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2021-10-04 09:55:59 +0200

    add: day06 challenge

---
+++ src/day06/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+)
+
+func shuffle(s string) string {
+	var r, even, odd string
+
+	for i, l := range s {
+		if i%2 == 0 {
+			even += string(l)
+		} else {
+			odd += string(l)
+		}
+	}
+	r = even + " " + odd
+	return r
+}
+
+func main() {
+	var (
+		T int
+		S string
+		output  []string
+	)
+
+	fmt.Scan(&T)
+
+	for i := 0; i < T; i++ {
+		fmt.Scan(&S)
+		output = append(output, shuffle(S))
+	}
+
+	for _,line := range output {
+		fmt.Println(line)
+	}
+}
