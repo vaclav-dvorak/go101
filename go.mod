e67b605397aa4885dd28d5058ebb18c71972911f
Vaclav Dvorak <vaclav.dvorak@bootiq.io>
2022-08-30 15:07:02 +0200

    add: mikrocviceni na rozvrstvedi nakupu v roce

--- go.mod
+++ go.mod
@@ -2,9 +2,13 @@ module github.com/vaclav-dvorak/go101
 
 go 1.18
 
-require github.com/spf13/cobra v1.2.1
+require (
+	github.com/sirupsen/logrus v1.9.0
+	github.com/spf13/cobra v1.2.1
+)
 
 require (
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
---
+++ src/invcalc/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+)
+
+const (
+	nakup = 700
+	dokup = 6000
+)
+
+var (
+	norm  = map[int]bool{1: true, 32: true, 60: true, 91: true, 121: true, 152: true, 182: true, 213: true, 244: true, 274: true, 305: true, 335: true}
+	prest = map[int]bool{1: true, 32: true, 61: true, 92: true, 122: true, 153: true, 183: true, 214: true, 245: true, 275: true, 306: true, 336: true}
+	nacon = map[int]bool{0: true, 3: true}
+)
+
+func main() {
+	balance := 6000
+	year := norm
+	for d := 1; d <= 365; d++ {
+		if _, ok := nacon[d%7]; ok {
+			balance -= nakup
+			if balance<0 {
+				log.Fatal("bust below zero")
+			}
+			log.Infof("%02d: nakupuju %d; bal %d", d, nakup, balance)
+		}
+		if _, ok := year[d]; ok {
+			balance += dokup
+			log.Infof("%02d: dokup %d; bal %d", d, dokup, balance)
+		}
+	}
+}
